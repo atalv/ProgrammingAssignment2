710b4a8c3b5f4fec76c2d5381de71f8b7374e227
atalv <droidatal@gmail.com>
2015-02-22 23:55:56 +0530

    Updated comments
    Added comments and example run statements

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall
+## Use makeCacheMatrix function with input as a invertible matrix.
+## Like: cm<-makeCacheMatrix(matrix(0:3,2,2))
+## Now cacheSolve(cm) - this function will return the inverse of the matrix 
+## defined above. It will caclculate the inverse if that matrix had not been inverted before,
+## else it will not calculate, rather will get the result from previous run.
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## This function uses the feature of lexical scoping and use of '<<-'
+## to assign a value which can be used outside current function's environment.
+## It helps it to retain the inverse value (to be calculated in the following funtion)
+## and use that. Whenever the input matrix is changed with cm$set, inverse value is again set
+## to null and will be recalculated in the following function.
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,7 +27,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## This function first recalls whether inverse of same matrix is requested from 
+## the previous run. If it is, then just prints a message and the result.
+## Else, it calculates the inverse and again stores that value for future use.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -32,3 +44,11 @@ cacheSolve <- function(x, ...) {
   x$setinv(i)
   i
 }
+
+## Example
+## cm<-makeCacheMatrix(matrix(0:3,2,2))
+## cacheSolve(cm)
+## cacheSolve(cm)
+## cm$set(matrix(1:4,2,2))
+## cacheSolve(cm)
+## cacheSolve(cm)
